72ba098ccfba300424f9a372b192f3f1b135cbbf
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-06-01 02:18:28 +0700

    styling comment

--- src/Screen/Comment/screen-comment.js
+++ src/Screen/Comment/screen-comment.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Container, Content, Text} from 'native-base';
 import Head from '../../Components/Head';
-import {View} from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {StyleSheet, View} from "react-native";
 
 function mapStateToProps(state) {
     return {};
@@ -16,19 +17,9 @@ class ScreenComment extends Component {
                       leftPress={() => this.props.navigation.goBack()} body={"Comment"}/>
                 <Content>
                     <View style={{margin: 10}}>
-                        <View style={{
-                            left: 10 + "%",
-
-                            width: 90 + "%",
-                            backgroundColor: '#FFFFFF',
-                            position: 'relative',
-                            borderRadius: 5
-                        }}>
-                            <View style={{
-                                marginLeft: 25,
-                                minHeight:60
-                            }}>
-                                <Text style={{color: '#2196F3', fontWeight: 'bold', fontSize: 14}}>
+                        <View style={styles.card}>
+                            <View style={styles.card_content}>
+                                <Text style={styles.card_name}>
                                     Yosafat Bama Arintoko
                                 </Text>
                                 <Text style={{fontSize: 12}}>
@@ -37,76 +28,73 @@ class ScreenComment extends Component {
                                     imperdiet vestibulum. Proin sit amet aliquam leo. Vestibulum scelerisque aliquam mi,
                                     a finibus nunc scelerisque nec.
                                 </Text>
-                                <Text style={{fontSize: 12}}>
-                                    last seen :
-                                </Text>
+                                <Icon name="clock-o" size={12} color='#BEBEBE' style={{justifyContent: 'center'}}>
+                                    <Text style={{fontSize: 12, color: '#BEBEBE'}}>
+                                        {' '}2 minute
+                                    </Text>
+                                </Icon>
                             </View>
                         </View>
-                        <View style={{height: 50, width: 50, backgroundColor: '#BEBEBE', borderRadius: 5, position:'absolute'}}>
+                        <View style={{
+                            height: 50,
+                            width: 50,
+                            backgroundColor: '#BEBEBE',
+                            borderRadius: 5,
+                            position: 'absolute'
+                        }}>
 
                         </View>
                     </View>
                     <View style={{margin: 10}}>
-                        <View style={{
-                            left: 10 + "%",
-
-                            width: 90 + "%",
-                            backgroundColor: '#FFFFFF',
-                            position: 'relative',
-                            borderRadius: 5
-                        }}>
-                            <View style={{
-                                marginLeft: 25,
-                                minHeight:60
-                            }}>
-                                <Text style={{color: '#2196F3', fontWeight: 'bold', fontSize: 14}}>
-                                    Yosafat Bama Arintoko
+                        <View style={styles.card}>
+                            <View style={styles.card_content}>
+                                <Text style={styles.card_name}>
+                                    Sinatrio Happy Triaji
                                 </Text>
                                 <Text style={{fontSize: 12}}>
-                                    Etiam et mollis ipsum. Sed porta varius magna, lacinia hendrerit massa viverra at.
-                                    Aenean vitae nibh augue. Proin eleifend accumsan nisi, vitae consectetur felis
-                                    imperdiet vestibulum. Proin sit amet aliquam leo. Vestibulum scelerisque aliquam mi,
-                                    a finibus nunc scelerisque nec.
+                                    Ut nec tortor quis tellus euismod auctor sed eget lacus. Sed at scelerisque massa,
+                                    nec egestas urna. Cras sit amet elementum arcu, ac luctus libero.
                                 </Text>
-                                <Text style={{fontSize: 12}}>
-                                    last seen :
-                                </Text>
+                                <Icon name="clock-o" size={12} color='#BEBEBE' style={{justifyContent: 'center'}}>
+                                    <Text style={{fontSize: 12, color: '#BEBEBE'}}>
+                                        {' '}2 minute
+                                    </Text>
+                                </Icon>
                             </View>
                         </View>
-                        <View style={{height: 50, width: 50, backgroundColor: '#BEBEBE', borderRadius: 5, position:'absolute'}}>
+                        <View style={{
+                            height: 50,
+                            width: 50,
+                            backgroundColor: '#BEBEBE',
+                            borderRadius: 5,
+                            position: 'absolute'
+                        }}>
 
                         </View>
                     </View>
-                    {/*<View style={{margin: 10, marginTop:40}}>*/}
-                        {/*<View style={{*/}
-                            {/*left: 10 + "%",*/}
-                            {/*height: 60,*/}
-                            {/*width: 90 + "%",*/}
-                            {/*backgroundColor: '#FFFFFF',*/}
-                            {/*position: 'absolute',*/}
-                            {/*borderRadius: 5*/}
-                        {/*}}>*/}
-                            {/*<View style={{*/}
-                                {/*marginLeft: 25*/}
-                            {/*}}>*/}
-                                {/*<Text style={{color: '#2196F3'}}>*/}
-                                    {/*aaa*/}
-                                {/*</Text>*/}
-                                {/*<Text style={{fontSize: 12}}>*/}
-                                    {/*last seen :*/}
-                                {/*</Text>*/}
-                            {/*</View>*/}
-                        {/*</View>*/}
-                        {/*<View style={{height: 50, width: 50, backgroundColor: '#BEBEBE', borderRadius: 5}}>*/}
-
-                        {/*</View>*/}
-                    {/*</View>*/}
                 </Content>
             </Container>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    card: {
+        left: 10 + "%",
+        width: 90 + "%",
+        backgroundColor: '#FFFFFF',
+        position: 'relative',
+        borderRadius: 5
+    },
+    card_content: {
+        marginLeft: 25,
+        minHeight: 60,
+        padding: 5
+    },
+    card_name: {
+        color: '#2196F3', fontWeight: 'bold', fontSize: 14
+    }
+});
 export default connect(
     mapStateToProps,
 )(ScreenComment);
